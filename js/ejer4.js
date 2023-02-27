e2a789c139cece082febe8ece64a381dee9d4ef6
Martinserrano724 <martinserrano724@gmail.com>
2023-02-27 10:03:41 -0300

    Creacion del ejercicio 4

---
+++ js/ejer4.js
@@ -0,0 +1,4 @@
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+let usuario = prompt ('¿Como te llamas?');
+
+document.write('hola '+usuario)
