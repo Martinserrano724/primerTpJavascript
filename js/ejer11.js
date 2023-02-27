0a3d91f763359032bd29c32fc559de0a9d226456
Martinserrano724 <martinserrano724@gmail.com>
2023-02-27 20:09:16 -0300

    correccion de errores de mostrar los datos en document.write

--- js/ejer11.js
+++ js/ejer11.js
@@ -1,21 +1,21 @@
 //11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible
 //(hay que decir todos por los que es divisible)
 
-let num1 = parseInt(prompt("¿Ingrese numero 1?"));
+let num1 = parseInt(prompt("¿Ingrese numero ?"));
 
-let valor = "el numero es divisible en: ";
+let valor = `el numero: ${num1} es divisible en: `;
 
 if (num1 % 2 == 0) {
-  valor += " ,2";
+  valor += " 2,";
 }
 if (num1 % 3 == 0) {
-  valor += " ,3";
+  valor += " 3,";
 } 
 if (num1 % 5 == 0) {
-    valor += " ,5";
+    valor += " 5,";
 } 
 if (num1 % 7 == 0) {
-      valor += " ,7";
+      valor += " 7,";
 } 
 if ((num1 % 7 != 0)&&(num1 % 5 != 0)&&(num1 % 3 != 0)&&(num1 % 2 != 0)){
       document.write("el numero:" + num1 + " No Divisible en 2,3,5,7 ");
