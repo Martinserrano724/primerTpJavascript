e7311037ed1c7988312aaceab01bca43391f1a19
Martinserrano724 <martinserrano724@gmail.com>
2023-02-27 10:07:15 -0300

    creacion del ejercicio 8

---
+++ js/ejer8.js
@@ -0,0 +1,10 @@
+//8.- Escribe un programa que pida un número y diga si es divisible por 2
+let num1 = parseInt(prompt ('¿Ingrese numero 1?'));
+
+
+if((num1 % 2)==0){
+    document.write('el numero:'+num1+' Divisible en 2 ');
+}
+else{
+    document.write('el numero:'+num1+' No Divisible en 2 ');
+}
